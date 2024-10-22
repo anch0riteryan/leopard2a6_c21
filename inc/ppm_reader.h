fdb39aa200058052791efcaf578a5d82b9ce7639
ryan <anch0rite.ryan@gmail.com>
2024-10-22 22:32:42 +0800

    ppm width calc update

--- inc/ppm_reader.h
+++ inc/ppm_reader.h
@@ -2,11 +2,14 @@
 
 #define TCC_PERIOD_US (16000 * 3) //60hz(t = 16ms) and 3cycle without update
 
-#define PPM_WIDTH_MAX_US 1940
-#define PPM_WIDTH_MIN_US 1140
+#define PPM_WIDTH_NEUTRO_US 1520
+#define PPM_WIDTH_MAX_US 1920
+#define PPM_WIDTH_MIN_US 1120
+#define PPM_WIDTH_GAP_US 19
 
 void init_ppm_reader (void );
 float ppm_phase_linear (uint32_t );
+float ppm_phase_joystick (uint32_t );
 float ppm_phase_digital (uint32_t );
 
 extern volatile uint32_t ppm_width_raw[8];
--- src/ppm_phaser.c
+++ src/ppm_phaser.c
@@ -1,8 +1,28 @@
 #include <ppm_reader.h>
 
+const float PPM_RANGE = (float) (PPM_WIDTH_MAX_US - PPM_WIDTH_MIN_US);
+
 float ppm_phase_linear (uint32_t width) {
 	float result = 0.0;
 
+	if (width < PPM_WIDTH_MIN_US) {
+		result = 0;
+	} else if (width > PPM_WIDTH_MAX_US) {
+		result = 1;
+	} else {
+		result = (width - PPM_WIDTH_MIN_US) / PPM_RANGE;
+	}
+
+	return result;
+}
+
+float ppm_phase_joystick (uint32_t width) {
+	float result = 0.0;
+
+	if (width > PPM_WIDTH_NEUTRO_US) {
+	} else if (width < PPM_WIDTH_NEUTRO_US) {
+	} else {
+	}
 
 	return result;
 }
